236c4153c22bb123f575535189c645c75183a8c9
Khushvinder.Pal <Khushvinder.Pal@dev-dell-daya.india.rsystems.com>
2015-03-10 17:41:42 +0530

    kjkh

--- src/com/example/githubtest/MainActivity.java
+++ src/com/example/githubtest/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//klgdfkghdfkhkdfhjkhhjfj
+		//gfhjklj
 		setContentView(R.layout.activity_main);
 	}
 }
